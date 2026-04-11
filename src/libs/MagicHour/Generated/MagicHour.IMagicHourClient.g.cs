11909f6236aa4997864a8d7853b254ce841959ee
Konstantin S. <havendv@gmail.com>
2026-04-11 06:38:59 +0400

    feat: Updated OpenAPI spec (#14)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/MagicHour/Generated/MagicHour.IMagicHourClient.g.cs
+++ src/libs/MagicHour/Generated/MagicHour.IMagicHourClient.g.cs
@@ -43,11 +43,11 @@ namespace MagicHour
         /// ensuring <see cref="ApiException.ResponseBody"/> is populated.
         /// </summary>
         public bool ReadResponseAsString { get; set; }
-
         /// <summary>
         /// Client-wide request defaults such as headers, query parameters, retries, and timeout.
         /// </summary>
         public global::MagicHour.AutoSDKClientOptions Options { get; }
+
 
         /// <summary>
         /// 
--- src/libs/MagicHour/Generated/MagicHour.VideoProjectsClient.g.cs
+++ src/libs/MagicHour/Generated/MagicHour.VideoProjectsClient.g.cs
@@ -4,7 +4,7 @@
 namespace MagicHour
 {
     /// <summary>
-    /// API related to video projects.<br/>
+    /// API related to video projects<br/>
     /// If no httpClient is provided, a new one will be created.<br/>
     /// If no baseUri is provided, the default baseUri from OpenAPI spec will be used.
     /// </summary>
